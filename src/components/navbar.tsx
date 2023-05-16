b60821cb1a8a0fea8416b0294507349abd740a4d
gael <76548719+gaelzarco@users.noreply.github.com>
2023-05-15 18:00:13 -0700

    sign-out, user avatar, drop-down integration ✅

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,46 @@
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/dropdown-menu"
+
+  import { ChevronDownIcon, PersonIcon, ExitIcon } from "@radix-ui/react-icons"
 
 export default function Navbar() {
+    const { data: session } = useSession()
+
     return (
-        <div className="inline-flex justify-between p-4 w-full">
+        <div className="inline-flex justify-between p-4 w-full items-center">
             <div className="inline-flex justify-self-start font-semibold text-md">
-                <h2>MedSpeak</h2>
+                {session && session.user && (
+                    <div className="inline-flex items-center">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="inline-flex w-max items-center my-auto focus-visible:outline-none">
+                                <Avatar>
+                                    <AvatarImage src={session.user.image as string} />
+                                    <AvatarFallback>{session.user.name?.toUpperCase().slice(0, 2)}</AvatarFallback>
+                                </Avatar>
+                                <p className="pl-4">{session.user.name}</p>
+                                <ChevronDownIcon className="ml-4 h-4 w-4" />
+                            </DropdownMenuTrigger>
+
+                            <DropdownMenuContent className="border-neutral-800">
+                                <DropdownMenuItem className="hover:bg-neutral-900">
+                                    <div className="inline-flex items-center justify-between w-full h-full">
+                                        <p>My Account</p>
+                                        <PersonIcon className="h-4 w-4" />
+                                    </div>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="hover:bg-neutral-900" onClick={() => signOut()}>
+                                    <div className="inline-flex items-center justify-between w-full h-full">
+                                        <p>Sign-Out</p>
+                                        <ExitIcon className="h-4 w-4" />
+                                    </div>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                )}
             </div>
             <div className="inline-flex justify-self-right text-sm">
                 <Link className='ps-8' href="dashboard">
@@ -12,9 +48,6 @@ export default function Navbar() {
                 </Link>
                 <Link className='ps-8' href='analytics'>
                     <p>Analytics</p>
-                </Link>
-                <Link className='ps-8' href="account">
-                    <p>Account</p>
                 </Link>
             </div>
         </div>
