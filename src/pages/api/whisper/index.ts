5ea52dc62e9f7887a9583d5a60e18003f0d089dd
gael <76548719+gaelzarco@users.noreply.github.com>
2023-05-16 14:24:59 -0700

    boilerplate API route for whisper transcription

--- src/pages/api/whisper/index.ts
+++ src/pages/api/whisper/index.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getTranscript } from '@/utils/openai'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-    const { body } = req.query;
+  const form = req.body
+
+  const title = form.title as string
+  const audioFile = form.recording as File
+
+  console.log(title)
+
+  const transcript = await getTranscript(audioFile)  
+
+  res.send({ transcript })
 }
