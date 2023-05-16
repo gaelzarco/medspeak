57aa5759f794245562efa34e80ff1f945405b943
gael <76548719+gaelzarco@users.noreply.github.com>
2023-05-15 18:02:18 -0700

    nextauth initialized ✅

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        // OAuth authentication providers...
+        // GitHubProvider({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string
+        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string
+        })
+        // ...add more providers here
+    ],
+})
