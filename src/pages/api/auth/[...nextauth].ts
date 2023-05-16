3c77b3cec0a5107a2d202c8e1cd895d4d3f3eea4
gael <76548719+gaelzarco@users.noreply.github.com>
2023-05-15 18:13:46 -0700

    unused code commented out 🫥

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import GitHubProvider from "next-auth/providers/github"
+// import GitHubProvider from "next-auth/providers/github"
 
 export default NextAuth({
     // Configure one or more authentication providers
